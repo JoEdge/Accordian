7343106ae2384d449f7759256fccd95fb280b374
Joanna Edgell <joedgell2013@gmail.com>
2014-10-02 19:35:25 -0400

    trying if/else

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,14 @@
 
 
-$('.box1').click(function(){
+if ($('.header1').is('click') ) {
   $('.box2').remove();
-
   $('.box3').remove();
-
-});
+} else if
+  ($('.header2').is('click') ) {
+    $('.box1').remove();
+    $('.box3').remove();
+} else if
+   ($('.header3').is('click') ) {
+    $('.box1').remove();
+    $('.box2').remove();
+};
